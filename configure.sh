314f30afd279c5bd9e9c25a86cc4d7507de67819
Pedro Sandoval <psandovalsegura@gmail.com>
2019-12-27 21:31:58 -0800

    Add grad month, acronym, and semesters

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Set proper permissions for directories and files
+mv * ..
+chmod 755 ../img/ ../javascripts/ ../projects/ ../stylesheets
+chmod 644 ../index.html ../params.json ../privacypolicy.htm 
